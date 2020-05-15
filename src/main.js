40e18d98962c4019f1183a7a7f127031b8565749
Dana <kovach.dana@yandex.ru>
2020-05-15 17:36:33 +0300

    Добавила надпись loading...

--- src/main.js
+++ src/main.js
@@ -3,10 +3,11 @@ import MenuComponent from "./components/menu";
 import FooterStatisticComponent from "./components/footer-statistic";
 import FilmsBlockComponent from "./components/films-block";
 import StatisticComponent from "./components/statistic";
+import LoadingComponent from "./components/loading";
 import PageController from "./controllers/page-controller";
 import FilterController from "./controllers/filter-controller";
 import FilmsModel from "./models/movies";
-import {RenderPosition, render} from "./utils/render";
+import {RenderPosition, render, remove} from "./utils/render";
 import API from "./api";
 
 const siteMainElement = document.querySelector(`.main`);
@@ -26,16 +27,18 @@ const filterController = new FilterController(siteMenuElement.getElement(), film
 filterController.render();
 
 const filmsBlock = new FilmsBlockComponent();
+
+const loadingComponent = new LoadingComponent();
+render(filmsBlock.getElement(), loadingComponent, RenderPosition.AFTERBEGIN);
+
 const pageController = new PageController(filmsBlock, filmsModel);
 
 render(siteMainElement, filmsBlock, RenderPosition.BEFOREEND);
 
 const footerStatisticElement = footerElement.querySelector(`.footer__statistics`);
 
-
 // TODO: переписать статистику на контроллер
 const statisticComponent = new StatisticComponent(filmsModel.getFilmsAll());
-statisticComponent.hide();
 
 siteMenuElement.setStatsClickHandler((evt) => {
   evt.preventDefault();
@@ -55,11 +58,15 @@ api.getFilms()
     filmsModel.setFilms(films);
   })
   .finally(() => {
+    remove(loadingComponent);
+
     const allFilms = filmsModel.getFilmsAll();
 
     pageController.render();
 
     render(siteHeaderElement, new UserRatingComponent(allFilms), RenderPosition.BEFOREEND);
     render(footerStatisticElement, new FooterStatisticComponent(allFilms), RenderPosition.BEFOREEND);
+
     render(siteMainElement, new StatisticComponent(allFilms), RenderPosition.BEFOREEND);
+    statisticComponent.hide();
   });
